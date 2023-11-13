0c4a954b1e516f15d7b55e07380b3c8c50d9c14c
jimkao <Jim87as0821@gmail.com>
2023-11-13 21:40:00 +0800

    第六周作業

---
+++ main.js
@@ -0,0 +1,224 @@
+import axios from "axios";
+
+let data = [];
+// 第一種
+// axios
+//   .get(
+//     "https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json"
+//   )
+//   .then((res) => {
+//     res.data.forEach((item) => {
+//       data.push(item);
+//     });
+//     renderData();
+//   });
+//第二種
+axios
+  .get(
+    "https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json"
+  )
+  .then((res) => {
+    res.data.data.forEach((item) => {
+      data.push(item);
+    });
+    renderData();
+  });
+function renderData() {
+  const areaData = [
+    "台北",
+    "新北",
+    "桃園",
+    "台中",
+    "臺南",
+    "高雄",
+    "新竹縣",
+    "苗栗縣",
+    "彰化縣",
+    "南投縣",
+    "雲林縣",
+    "嘉義縣",
+    "屏東縣",
+    "宜蘭縣",
+    "花蓮縣",
+    "臺東縣",
+    "澎湖縣",
+    "金門縣",
+    "連江縣",
+    "基隆市",
+    "新竹市",
+    "嘉義市",
+  ];
+
+  const list = document.querySelector(".ticketCard-area");
+  const regionSearch = document.querySelector(".regionSearch");
+  const searchResult = document.querySelector("#searchResult-text");
+  // 新增套票變數
+  const ticketName = document.querySelector("#ticketName");
+  const ticketImgUrl = document.querySelector("#ticketImgUrl");
+  const ticketRegion = document.querySelector("#ticketRegion");
+  const ticketPrice = document.querySelector("#ticketPrice");
+  const ticketNum = document.querySelector("#ticketNum");
+  const ticketRate = document.querySelector("#ticketRate");
+  const ticketDescription = document.querySelector("#ticketDescription");
+  const addBtn = document.querySelector("#addBtn");
+
+  // 放入所有縣市
+  function areaOption() {
+    let str = `<option value="全部">全部地區</option>`;
+    let addStr = `<option value="" disabled selected hidden>請選擇景點地區</option>`;
+    areaData.forEach((item) => {
+      str += `
+      <option value="${item}">${item}</option>`;
+      addStr += `
+      <option value="${item}">${item}</option>`;
+    });
+    regionSearch.innerHTML = str;
+    ticketRegion.innerHTML = addStr;
+  }
+  areaOption();
+  // 初始化
+  function init() {
+    let str = "";
+    let searchLength = 0;
+    data.forEach((item) => {
+      str += ` <li class="ticketCard">
+      <div class="ticketCard-img">
+        <a href="#">
+          <img
+            src="${item.imgUrl}"
+            alt=""
+          />
+        </a>
+        <div class="ticketCard-region">${item.area}</div>
+        <div class="ticketCard-rank">${item.rate}</div>
+      </div>
+      <div class="ticketCard-content">
+        <div>
+          <h3>
+            <a href="#" class="ticketCard-name">${item.name}</a>
+          </h3>
+          <p class="ticketCard-description">
+            ${item.description}
+          </p>
+        </div>
+        <div class="ticketCard-info">
+          <p class="ticketCard-num">
+            <span><i class="fas fa-exclamation-circle"></i></span>
+            剩下最後 <span id="ticketCard-num"> ${item.group}</span> 組
+          </p>
+          <p class="ticketCard-price">
+            TWD <span id="ticketCard-price">${item.price}</span>
+          </p>
+        </div>
+      </div>
+    </li>`;
+      searchLength += 1;
+    });
+    list.innerHTML = str;
+    searchResult.innerHTML = `本次搜尋共 ${searchLength} 筆資料`;
+  }
+  init();
+
+  // 搜尋相對縣市條件
+  regionSearch.addEventListener("change", (e) => {
+    const hostCity = e.target.value;
+    hostCity === "全部" ? init() : filterArea(hostCity);
+    function filterArea(hostCity) {
+      let str = "";
+      let searchLength = 0;
+      data.forEach((item) => {
+        if (item.area === hostCity) {
+          str += ` <li class="ticketCard">
+          <div class="ticketCard-img">
+            <a href="#">
+              <img
+                src="${item.imgUrl}"
+                alt=""
+              />
+            </a>
+            <div class="ticketCard-region">${item.area}</div>
+            <div class="ticketCard-rank">${item.rate}</div>
+          </div>
+          <div class="ticketCard-content">
+            <div>
+              <h3>
+                <a href="#" class="ticketCard-name">${item.name}</a>
+              </h3>
+              <p class="ticketCard-description">
+                ${item.description}
+              </p>
+            </div>
+            <div class="ticketCard-info">
+              <p class="ticketCard-num">
+                <span><i class="fas fa-exclamation-circle"></i></span>
+                剩下最後 <span id="ticketCard-num"> ${item.group}</span> 組
+              </p>
+              <p class="ticketCard-price">
+                TWD <span id="ticketCard-price">${item.price}</span>
+              </p>
+            </div>
+          </div>
+        </li>`;
+          searchLength += 1;
+        }
+      });
+      list.innerHTML = str;
+      str = `本次搜尋共 ${searchLength} 筆資料`;
+      searchLength === 0
+        ? (searchResult.innerHTML = str + `,找不到相關頁面`)
+        : (searchResult.innerHTML = str);
+    }
+  });
+
+  // 判斷有沒有選項沒填寫或錯誤
+  function alertStation(valueArr) {
+    let alertLength = 0;
+    valueArr.forEach((item) => {
+      if (item.value.trim() === "") {
+        alertLength += 1;
+      }
+      if (
+        item === ticketRate &&
+        (Number(item.value.trim()) > 10 || Number(item.value.trim()) < 1)
+      ) {
+        alertLength += 1;
+      }
+    });
+    return alertLength;
+  }
+  //套票資訊清除
+  function cleanData(valueArr) {
+    valueArr.forEach((item) => {
+      item.value = "";
+    });
+  }
+  // 增加套票監聽
+  addBtn.addEventListener("click", (e) => {
+    const valueArr = [
+      ticketName,
+      ticketImgUrl,
+      ticketRegion,
+      ticketDescription,
+      ticketNum,
+      ticketPrice,
+      ticketRate,
+    ];
+    if (alertStation(valueArr) > 0) {
+      alert("請檢查每個選項是否填寫/正確");
+      return;
+    }
+    const idLength = data.length;
+    let obj = {};
+    obj.id = idLength;
+    obj.name = ticketName.value.trim();
+    obj.imgUrl = ticketImgUrl.value.trim();
+    obj.area = ticketRegion.value.trim();
+    obj.description = ticketDescription.value.trim();
+    obj.group = Number(ticketNum.value.trim());
+    obj.price = Number(ticketPrice.value.trim());
+    obj.rate = Number(ticketRate.value.trim());
+    data.push(obj);
+    init();
+    cleanData(valueArr);
+  });
+}
